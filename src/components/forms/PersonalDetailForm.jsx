0751714360f998030fcfca1ee4435407f8989ec0
abiraltamang <abiral.tamang816@gmail.com>
2023-06-16 18:16:49 +0545

    personal detail form added

--- src/components/forms/PersonalDetailForm.jsx
+++ src/components/forms/PersonalDetailForm.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
-import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
-import TextInput from './TextInput';
+import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import TextInput from "./TextInput";
+
+const bloodGroupOptions = [
+  { value: "A+", label: "A+" },
+  { value: "A-", label: "A-" },
+  { value: "B+", label: "B+" },
+  { value: "B-", label: "B-" },
+  { value: "AB+", label: "AB+" },
+  { value: "AB-", label: "AB-" },
+  { value: "O+", label: "O+" },
+  { value: "O-", label: "O-" },
+];
+
+const nearbyPlacesOptions = [
+  { value: "Park", label: "Park" },
+  { value: "Restaurant", label: "Restaurant" },
+  { value: "Shopping Mall", label: "Shopping Mall" },
+  { value: "Hospital", label: "Hospital" },
+];
 
 const PersonalDetailForm = () => {
   const initialValues = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    address: '',
-    phone: '',
+    firstName: "",
+    guardianName: "",
+    middleName: "",
+    lastName: "",
+    email: "",
+    address: "",
+    phone: "",
   };
 
   const validationSchema = Yup.object({
-    firstName: Yup.string().required('First name is required'),
-    lastName: Yup.string().required('Last name is required'),
-    email: Yup.string().email('Invalid email address').required('Email is required'),
-    address: Yup.string().required('Address is required'),
-    phone: Yup.string().required('Phone number is required'),
+    firstName: Yup.string().required("Required"),
+    lastName: Yup.string().required("Required"),
+    email: Yup.string().email("Invalid email address").required("Required"),
+    address: Yup.string().required("Required"),
+    phone: Yup.string().required("Required"),
   });
 
   const handleSubmit = (values) => {
@@ -26,25 +46,98 @@ const PersonalDetailForm = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto">
-      <h2 className="text-2xl font-bold mb-4">Personal Details</h2>
+    <div className="w-full bg-slate-100 px-8 mb-4 py-3 rounded-lg">
       <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
         onSubmit={handleSubmit}
       >
         <Form>
-          <TextInput type="text" id="firstName" name="firstName" label="First Name" />
-          <TextInput type="text" id="lastName" name="lastName" label="Last Name" />
-          <TextInput type="email" id="email" name="email" label="Email Address" />
-          <TextInput as="textarea" id="address" name="address" label="Address" />
-          <TextInput type="text" id="phone" name="phone" label="Phone Number" />
-          <button
-            type="submit"
-            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-          >
-            Submit
-          </button>
+          <h2 className="text-2xl font-medium mb-4">Applicant Name</h2>
+          <div className="grid grid-cols-2 gap-x-5">
+            <TextInput
+              type="text"
+              id="firstName"
+              name="firstName"
+              label="First Name"
+            />
+            <TextInput
+              type="text"
+              id="middleName"
+              name="middleName"
+              label="Middle Name"
+            />
+            <TextInput
+              type="text"
+              id="lastName"
+              name="lastName"
+              label="Last Name"
+            />
+          </div>
+          <h2 className="text-2xl font-medium mb-4 mt-3">Personal Details</h2>
+
+          <div className="grid grid-cols-2 gap-x-5">
+            <TextInput
+              type="date"
+              id="dateOfBirth"
+              name="dateOfBirth"
+              label="Date of Birth"
+            />
+            <TextInput
+              type="select"
+              id="gender"
+              name="gender"
+              label="Gender"
+              options={[
+                { value: "male", label: "Male" },
+                { value: "female", label: "Female" },
+                { value: "other", label: "Other" },
+              ]}
+            />
+            <TextInput
+              type="select"
+              id="bloodGroup"
+              name="bloodGroup"
+              label="Blood Group"
+              options={bloodGroupOptions}
+            />
+            <TextInput
+              type="select"
+              id="nearbyPlaces"
+              name="nearbyPlaces"
+              label="Nearby Places"
+              options={nearbyPlacesOptions}
+            />
+          </div>
+          <h2 className="text-2xl font-medium mb-4 mt-3">
+            Guardian Information
+          </h2>
+          <div className="grid grid-cols-2 gap-x-5">
+            <TextInput
+              type="text"
+              id="guardianName"
+              name="guardianName"
+              label="Name"
+            />
+            <TextInput
+              type="select"
+              id="gender"
+              name="gender"
+              label="Gender"
+              options={[
+                { value: "father", label: "Father" },
+                { value: "mother", label: "Mother" },
+              ]}
+            />
+          </div>
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+            >
+              Submit
+            </button>
+          </div>
         </Form>
       </Formik>
     </div>
--- src/components/forms/TextInput.jsx
+++ src/components/forms/TextInput.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useField, ErrorMessage } from 'formik';
+import React from "react";
+import { useField, ErrorMessage } from "formik";
 
 const TextInput = ({ label, ...props }) => {
   const [field, meta] = useField(props);
@@ -9,13 +9,29 @@ const TextInput = ({ label, ...props }) => {
       <label htmlFor={props.id || props.name} className="block font-medium mb-1">
         {label}
       </label>
-      <input
-        {...field}
-        {...props}
-        className={`border border-gray-300 rounded px-3 py-2 w-full ${
-          meta.touched && meta.error ? 'border-red-500' : ''
-        }`}
-      />
+      {props.type === "select" ? (
+        <select
+          {...field}
+          {...props}
+          className={`border border-gray-300 bg-white rounded px-3 py-3 w-full ${
+            meta.touched && meta.error ? "border-red-500" : ""
+          }`}
+        >
+          {props.options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      ) : (
+        <input
+          {...field}
+          {...props}
+          className={`border border-gray-300 rounded px-3 py-2  w-full ${
+            meta.touched && meta.error ? "border-red-500" : ""
+          } ${props.type === "date" ? "" : ""}`}
+        />
+      )}
       <ErrorMessage name={field.name} component="p" className="text-red-500" />
     </div>
   );
